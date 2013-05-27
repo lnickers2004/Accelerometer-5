9240b162fe01d5b5f135383f7e89aed3d8131289
Кристиян Боянов <skylalker@abv.bg>
2013-05-27 03:48:49 -0500

    zdcsd

--- MyFirstApp/js/index.js
+++ MyFirstApp/js/index.js
@@ -1,50 +1,78 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
+//Activate :active state on device
+document.addEventListener("touchstart", function() {}, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+	var accelerometerHelper = new AccelerometerApp();
+	accelerometerHelper.run();
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+function AccelerometerApp() {
+
+}
+
+AccelerometerApp.prototype = {
+	watchID : null,
+    direction: null, 
+	run: function() {
+		var that = this,
+    		startButton = document.getElementById("startButton"),
+    		stopButton = document.getElementById("stopButton");
+    		that.direction = document.getElementById("direction");
+
+		startButton.addEventListener("click", 
+									 function() { 
+										 that._startWatch.apply(that, arguments)
+									 });
+		stopButton.addEventListener("click", 
+									function() { 
+										that._stopWatch.apply(that, arguments)
+									});
+	},
+    
+	// Start watching the acceleration
+	_startWatch: function() {
+		// Only start testing if watchID is currently null.
+		var that = this;
+		if (that.watchID === null) {
+			// Update acceleration every .5 second
+			var options = { frequency: 500 };
+			that.watchID = navigator.accelerometer.watchAcceleration(function() { 
+				that._onAccelerometerSuccess.apply(that, arguments)
+			}, 
+            function(error) { 
+             that._onAccelerometerError.apply(that, arguments)
+            }, 
+            options);
+		}
+	},
+     
+	// Stop watching the acceleration
+	_stopWatch: function() {
+		var that = this;
+		if (that.watchID !== null) {
+			navigator.accelerometer.clearWatch(that.watchID);
+			that.watchID = null;
+		}
+	},
+ 
+	//Get a snapshot of the current acceleration
+	_onAccelerometerSuccess: function(acceleration) {
+		var that = this;
+        if(acceleration.x*acceleration.y<0){
+            that.direction.innerText = "left";
+        }
+        else if(acceleration.x*acceleration.y>0){
+            that.direction.innerText = "right";
+        }
+		//that.spanX.innerText = acceleration.x;
+		//that.spanY.innerText = acceleration.y;
+		//that.spanZ.innerText = acceleration.z;              
+	},
+    
+	//Failed to get the acceleration
+	_onAccelerometerError: function(error) {
+		alert("Unable to start accelerometer! Error code: " + error.code );
+	}
+}
